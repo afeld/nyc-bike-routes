3230fb3328f231bb096323660f19d0ac2e5c6f97
Aidan Feldman <aidan.feldman@gmail.com>
2026-07-10 05:58:33 -0400

    rearrange

--- app_pages/miles.py
+++ app_pages/miles.py
@@ -6,10 +6,11 @@ from bike_routes.views import render_cumulative_miles, render_yearly_miles
 routes = load_routes()
 
 st.subheader("Miles added by year")
-st.markdown(
-    "Route length is measured in the [EPSG:2263 coordinate system](https://epsg.io/2263) and converted from feet to miles."
-)
 render_yearly_miles(routes)
 
 st.subheader("Network size")
 render_cumulative_miles(routes)
+
+st.markdown(
+    "Route length is measured in the [EPSG:2263 coordinate system](https://epsg.io/2263) and converted from feet to miles."
+)
